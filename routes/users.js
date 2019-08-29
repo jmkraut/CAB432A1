d1a1496261d261b11a799516b22694a5b367f62b
JKraut <jacobmatthewkraut@hotmail.com>
2019-08-29 15:41:18 +1000

    Added a few more necessary npm packages.

--- routes/users.js
+++ routes/users.js
@@ -3,7 +3,18 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  const theHtml = `
+<html>
+<head><title>My First SSR</title></head>
+<body>
+<h1>My First Server Side Render</h1>
+<div id="reactele">{{{reactele}}}</div>
+<script src="/app.js" charset="utf-8"></script>
+<script src="/vendor.js" charset="utf-8"></script>
+</body>
+</html>
+`;
+res.send(theHtml);
 });
 
 module.exports = router;
