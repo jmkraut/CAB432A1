017b66ddc64362e853e66adc96d43e7251c75853
JKraut <jacobmatthewkraut@hotmail.com>
2019-08-29 16:45:21 +1000

    Removed some unnecessary files and added some basic axios requests.

--- app.js
+++ app.js
@@ -1,17 +1,12 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const app = express();
+const axios = require('axios');
+let summary = "";
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-
-var app = express();
-
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -19,25 +14,24 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+axios.get("https://api.ipdata.co/?api-key=74dc719f974815bd528cf30c7bc844f6bbf550b4357db6dd5537bae1")
+.then(function (response) {
+  axios.get("https://api.darksky.net/forecast/8871d1e0a911accaa06df49bd016b42e/" + 
+  response.data.latitude + "," + response.data.longitude)
+  .then(function(res) {
+    summary = res.data.currently.summary
+  })
+  .catch(function(err){
+    console.log(err)
+  })
+})
+.catch(function(error){
+  console.log(error);
+})
 
-
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
-});
-
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
+app.get('/', function (req, res) {
+  res.send(summary)
+})
 
 app.listen(3000, () => 
   console.log("Listening on port 3000!")
