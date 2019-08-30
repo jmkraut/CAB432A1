0148d1c7726ee61201277692a4033b533305c823
Jacob <jacob.kraut@connect.qut.edu.au>
2019-08-30 20:14:07 +1000

    Created separated folders for server and client and created basic react app.

--- src/server/app.js
+++ src/server/app.js
@@ -1,21 +1,23 @@
-const createError = require('http-errors');
 const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
-const app = express();
+const server = express();
 const axios = require('axios');
+const bodyParser = require('body-parser')
 let summary = "";
 
 
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+server.use(logger('dev'));
+server.use(express.json());
+server.use(express.urlencoded({ extended: false }));
+server.use(bodyParser.urlencoded({ extended: false }))
+server.use(bodyParser.json())
+server.use(cookieParser());
+server.use(express.static(path.join(__dirname, 'public')));
 
 
-app.get('/', function (req, res) {
+server.get('/', function (req, res) {
 
   axios.get("https://api.ipdata.co/?api-key=74dc719f974815bd528cf30c7bc844f6bbf550b4357db6dd5537bae1")
 
@@ -25,14 +27,6 @@ app.get('/', function (req, res) {
 
     .then(function(res) {
       summary = res.data.currently.summary
-
-      .then(function(res) {
-        axios.get("https://app.ticketmaster.com/discovery/v2/events.json?apikey=4OMcMtE7RsqOGgvSIuMpVPKQMmf4IHib&latlong=" + res.data.latitude + "," + res.data.longitude)
-      })
-
-      .catch(function(err){
-        console.log(err)
-      })
     })
     .catch(function(err){
       console.log(err)
@@ -46,8 +40,10 @@ app.get('/', function (req, res) {
 
 })
 
-app.listen(3000, () => 
+server.listen(3000, () => 
   console.log("Listening on port 3000!")
 )
 
-module.exports = app;
+module.exports = server;
+
+//axios.get("https://app.ticketmaster.com/discovery/v2/events.json?apikey=4OMcMtE7RsqOGgvSIuMpVPKQMmf4IHib&latlong=" + res.data.latitude + "," + res.data.longitude)
