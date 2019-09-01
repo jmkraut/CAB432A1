805b1bd23b70aa401754d03b3f876972d2060d5a
JKraut <jacobmatthewkraut@hotmail.com>
2019-09-01 22:44:07 +1000

    Completed more work on client side .

--- src/client/src/App.js
+++ src/client/src/App.js
@@ -1,26 +1,65 @@
-import React from 'react';
-import logo from './logo.svg';
+import React from 'react'
 import './App.css';
+import Navbar from 'react-bootstrap/Navbar'
+import Image from 'react-bootstrap/Image'
+import Container from 'react-bootstrap/Container'
+import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/Col'
+import ds from './ds.png'
+import ipdata from './favicon.ico'
+import tm from './tm.png'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+function App(){
+  return(
+  <div>
+    {/* Top Navbar Construction */}
+      <Navbar bg="dark" variant="dark" className="Topbar">
+    <Navbar.Brand>
+      EventWeather
+    </Navbar.Brand>
+  </Navbar>
+
+  {/* Bottom Navbar Construction */}
+  <Navbar bg="dark" variant="dark" fixed="bottom">
+    <Navbar.Brand>
+
+  {/* Container to hold the API powered by images */}
+  <Container>
+  <Row>
+    
+    {/* Darksky API */}
+    <Col xs={3} md={3} lg={2} sm={3}>
+      <Image
+      className="darksky"
+      src={ds}
+      alt={"ds"}
+      fluid/>
+    </Col>
+
+    {/* IPData.co API */}
+    <Col xs={2} md={2} lg={1} sm={2}>
+      <Image
+      className="ipdata"
+      src={ipdata}
+      alt={"ipdata"}
+      fluid/>
+    </Col>
+
+    {/* Ticketmaster API */}
+    <Col xs={3} md={3} lg={3} sm={3}>
+      <Image
+      className="tm"
+      src={tm}
+      alt={"tm"}
+      fluid/>
+    </Col>
+  </Row>
+</Container>
+</Navbar.Brand>
+  </Navbar>
+  </div>
+  )
 }
 
 export default App;
