30fb0846b0e2e153fbbec2e96da1fa2dcb913c85
Jacob <jacob.kraut@connect.qut.edu.au>
2019-09-06 00:01:56 +1000

    Playing around with cards and containers.

--- src/client/src/App.js
+++ src/client/src/App.js
@@ -1,65 +1,65 @@
-import React from 'react'
-import './App.css';
-import Navbar from 'react-bootstrap/Navbar'
-import Image from 'react-bootstrap/Image'
-import Container from 'react-bootstrap/Container'
-import Row from 'react-bootstrap/Row'
-import Col from 'react-bootstrap/Col'
-import ds from './images/ds.png'
-import ipdata from './images/favicon.ico'
-import tm from './images/tm.png'
+import React from "react";
+import "./App.css";
+import Navbar from "react-bootstrap/Navbar";
+import Image from "react-bootstrap/Image";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import Card from "react-bootstrap/Card";
+import ds from "./images/ds.png";
+import ipdata from "./images/favicon.ico";
+import tm from "./images/tm.png";
 
+function App() {
+  return (
+    <div>
+      {/* Top Navbar Construction */}
+      <Navbar className="Topbar" bg="dark" variant="dark">
+        <Navbar.Brand>
+          <h1>EventWeather</h1>
+        </Navbar.Brand>
+      </Navbar>
+      <Container>
+        <Row>
+          <Card className="CardTest" style={{ width: "18rem" }}>
+            <Card.Img variant="top" src={ipdata} />
+            <Card.Body>
+              <Card.Title>Card Title</Card.Title>
+              <Card.Text>
+                Some quick example text to build on the card title and make up
+                the bulk of the card's content.
+              </Card.Text>
+            </Card.Body>
+          </Card>
+        </Row>
+      </Container>
 
-function App(){
-  return(
-  <div>
-    {/* Top Navbar Construction */}
-      <Navbar bg="dark" variant="dark" className="Topbar">
-    <Navbar.Brand>
-      <h1>EventWeather</h1>
-    </Navbar.Brand>
-  </Navbar>
+      {/* Bottom Navbar Construction */}
+      <Navbar bg="dark" variant="dark" fixed="bottom">
+        <Navbar.Brand>
+          {/* Container to hold the API powered by images */}
+          <Container>
+            <Row>
+              {/* Darksky API */}
+              <Col xs={3} md={3} lg={2} sm={3}>
+                <Image className="darksky" src={ds} alt={"ds"} fluid />
+              </Col>
 
-  {/* Bottom Navbar Construction */}
-  <Navbar bg="dark" variant="dark" fixed="bottom">
-    <Navbar.Brand>
+              {/* IPData.co API */}
+              <Col xs={2} md={2} lg={1} sm={2}>
+                <Image className="ipdata" src={ipdata} alt={"ipdata"} fluid />
+              </Col>
 
-  {/* Container to hold the API powered by images */}
-  <Container>
-  <Row>
-    
-    {/* Darksky API */}
-    <Col xs={3} md={3} lg={2} sm={3}>
-      <Image
-      className="darksky"
-      src={ds}
-      alt={"ds"}
-      fluid/>
-    </Col>
-
-    {/* IPData.co API */}
-    <Col xs={2} md={2} lg={1} sm={2}>
-      <Image
-      className="ipdata"
-      src={ipdata}
-      alt={"ipdata"}
-      fluid/>
-    </Col>
-
-    {/* Ticketmaster API */}
-    <Col xs={3} md={3} lg={3} sm={3}>
-      <Image
-      className="tm"
-      src={tm}
-      alt={"tm"}
-      fluid/>
-    </Col>
-  </Row>
-</Container>
-</Navbar.Brand>
-  </Navbar>
-  </div>
-  )
+              {/* Ticketmaster API */}
+              <Col xs={3} md={3} lg={3} sm={3}>
+                <Image className="tm" src={tm} alt={"tm"} fluid />
+              </Col>
+            </Row>
+          </Container>
+        </Navbar.Brand>
+      </Navbar>
+    </div>
+  );
 }
 
 export default App;
