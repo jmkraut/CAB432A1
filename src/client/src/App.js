75c4a00374234ed888b11d6c55c707722d402d79
Jacob <jacob.kraut@connect.qut.edu.au>
2019-09-07 01:28:30 +1000

    Need to fix card component array.

--- src/client/src/App.js
+++ src/client/src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import ReactAnimatedWeather from "react-animated-weather";
 import "./App.css";
 import Navbar from "react-bootstrap/Navbar";
@@ -8,116 +8,121 @@ import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import Card from "react-bootstrap/Card";
 import Spinner from "react-bootstrap/Spinner";
+import CardColumns from "react-bootstrap/CardColumns";
 import ds from "./images/ds.png";
 import ipdata from "./images/favicon.ico";
 import tm from "./images/tm.png";
+
+let names = [];
+let images = [];
+let info = [];
+let urls = [];
+let venues = [];
+let cards = [];
 
 function App() {
   const [temp, setTemp] = useState("");
   const [city, setCity] = useState("");
   const [hidden, setHidden] = useState(false);
   const [title, setTitle] = useState("");
+  const [image, setImage] = useState("");
+  const [icon, setIcon] = useState("");
 
-  fetch("http://localhost:3001/")
-    .then(res => res.json())
-    .then(res => {
-      setHidden(true)
-      setTemp(((res.darksky.currently.temperature - 32) / 1.8).toFixed(2))
-      setCity(res.ipdata.city)
-      setTitle(res.ticketmaster._embedded.events[2].name)
-    })
-    .catch(error => {
-      console.log(error);
-    });
-  
+  useEffect(() => {
+    fetch("http://localhost:3001/")
+      .then(res => res.json())
+      .then(res => {
+        for (let i = 0; i < res.ticketmaster._embedded.events.length; i++) {
+          names.push(res.ticketmaster._embedded.events[i].name);
+          images.push(res.ticketmaster._embedded.events[i].images[1].url);
+          info.push(res.ticketmaster._embedded.events[i].info);
+          urls.push(res.ticketmaster._embedded.events[i].url);
+          venues.push(res.ticketmaster._embedded.events[i]._embedded.venues[0].name);
+          
+        }
+        setHidden(true);
+        setTemp(((res.darksky.currently.temperature - 32) / 1.8).toFixed(0));
+        setCity(res.ipdata.city);
+        setTitle(res.ticketmaster._embedded.events[0].name);
+        setImage(res.ticketmaster._embedded.events[0].images[1].url);
+        setIcon(res.darksky.currently.icon.replace(/-/g, "_").toUpperCase());
+      })
+      .then(() => {
+            for (let i = 0; i < names.length; i++) {
+              cards.push(
+                <Card style={{ width: "18rem" }}>
+                  <Card.Header>
+                    <strong>{title}</strong>
+                  </Card.Header>
+                  <Card.Img variant="top" src={images[i]} />
+                  <br />
+                  <Card.Title>{venues[i]}</Card.Title>
+                  <Card.Body>
+                    <Card.Text>{info[i]}</Card.Text>
+                  </Card.Body>
+                  <Card.Footer>
+                    <Card.Link href={urls[i]}>{urls[i]}</Card.Link>
+                  </Card.Footer>
+                </Card>
+              );
+        }
+        
+        console.log(cards)
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  }, []);
 
   return (
     <div>
       {/* Top Navbar Construction */}
-      <Navbar className="Topbar" bg="dark" variant="dark" sticky="top">
+      <Navbar className="Topbar" bg="dark" variant="dark">
         <Navbar.Brand>
-          <h1>
-            EventWeather{" "}
-            <Spinner
-              className="WeatherSpinner"
-              animation="grow"
-              hidden={hidden}
-            />
-          </h1>
+          <ReactAnimatedWeather
+            icon={icon}
+            color="white"
+            size="128"
+            animate={true}
+          />
         </Navbar.Brand>
-        <Card className="WeatherCard" style={{ width: "18rem" }} hidden={true}>
-          <Card.Body>
-            <Card.Text>
-              <strong>{city}</strong>
-            </Card.Text>
-            <Card.Text>{temp} °C</Card.Text>
-          </Card.Body>
-        </Card>
+
+        <Navbar.Brand>
+            <ul style={{ listStyleType: "none" }}>
+              <li>
+                <h1>EventWeather</h1>
+                <h6>Events and weather for your day out.</h6>
+              </li>
+              <li>
+                <strong>{city}</strong>
+              </li>
+              <li>
+                <p>{temp} °C</p>
+              </li>
+            </ul>
+        </Navbar.Brand>
       </Navbar>
-      <Container>
-        <Row>
-          <Card className="CardTest" style={{ width: "18rem" }}>
-            <Card.Img variant="top" src={ipdata} />
-            <Card.Body>
-              <Card.Title>{title}</Card.Title>
-              <Card.Text>
-                Some quick example text to build on the card title and make up
-                the bulk of the card's content.
-              </Card.Text>
-            </Card.Body>
-          </Card>
-          <Card className="CardTest" style={{ width: "18rem" }}>
-            <Card.Img variant="top" src={ipdata} />
-            <Card.Body>
-              <Card.Title>Card Title</Card.Title>
-              <Card.Text>
-                Some quick example text to build on the card title and make up
-                the bulk of the card's content.
-              </Card.Text>
-            </Card.Body>
-          </Card>
-          <Card className="CardTest" style={{ width: "18rem" }}>
-            <Card.Img variant="top" src={ipdata} />
-            <Card.Body>
-              <Card.Title>Card Title</Card.Title>
-              <Card.Text>
-                Some quick example text to build on the card title and make up
-                the bulk of the card's content.
-              </Card.Text>
-            </Card.Body>
-          </Card>
-          <Card className="CardTest" style={{ width: "18rem" }}>
-            <Card.Img variant="top" src={ipdata} />
-            <Card.Body>
-              <Card.Title>Card Title</Card.Title>
-              <Card.Text>
-                Some quick example text to build on the card title and make up
-                the bulk of the card's content.
-              </Card.Text>
-            </Card.Body>
-          </Card>
-          <Card className="CardTest" style={{ width: "18rem" }}>
-            <Card.Img variant="top" src={ipdata} />
-            <Card.Body>
-              <Card.Title>Card Title</Card.Title>
-              <Card.Text>
-                Some quick example text to build on the card title and make up
-                the bulk of the card's content.
-              </Card.Text>
-            </Card.Body>
-          </Card>
-          <Card className="CardTest" style={{ width: "18rem" }}>
-            <Card.Img variant="top" src={ipdata} />
-            <Card.Body>
-              <Card.Title>Card Title</Card.Title>
-              <Card.Text>
-                Some quick example text to build on the card title and make up
-                the bulk of the card's content.
-              </Card.Text>
-            </Card.Body>
-          </Card>
-        </Row>
-      </Container>
+
+      {/* <Card style={{ width: "18rem" }}>
+        <Card.Header>
+          <strong>{title}</strong>
+        </Card.Header>
+        <Card.Img variant="top" src={images[0]} />
+        <br />
+        <Card.Title>{venues[0]}</Card.Title>
+        <Card.Body>
+          <Card.Text>{info[0]}</Card.Text>
+        </Card.Body>
+        <Card.Footer>
+          <Card.Link href={urls[0]}>{urls[0]}</Card.Link>
+        </Card.Footer>
+      </Card> */}
+      <React.Fragment>{cards}</React.Fragment>
+      <ol>
+        {cards.map(card => (
+          <li key={card}>{card}</li>
+        ))}
+      </ol>
 
       {/* Bottom Navbar Construction */}
       <Navbar bg="dark" variant="dark" fixed="bottom">
